e8c87abe74989bb1bdebe34947727d274f2cab3f
Ankur33657 <ankursingh943040@gmail.com>
2025-10-20 19:00:43 +0530

    .

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const userRouter = require("./src/routes/user");
 const notesRouter = require("./src/routes/notes");
 const cors = require("cors");
 
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const app = express();
+app.use(cookieParser());
 app.use(
   cors({
     origin: "https://note-frontend-37dy.onrender.com",
@@ -13,11 +18,6 @@ app.use(
 );
 
 app.options("*", cors());
-require("dotenv").config();
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const app = express();
-app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api", userRouter);
