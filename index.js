27ae13c72999d60bdc033a944bbfe2b4c9969ceb
Ankur33657 <ankursingh943040@gmail.com>
2025-10-20 18:56:04 +0530

    feat:add cors

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const userRouter = require("./src/routes/user");
 const notesRouter = require("./src/routes/notes");
 const cors = require("cors");
 
-const corsOptions = {
-  origin: ["http://localhost:3000", "https://note-frontend-37dy.onrender.com"],
+app.use(
+  cors({
+    origin: "https://note-frontend-37dy.onrender.com",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  }),
+);
 
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-};
+app.options("*", cors());
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use("*", cors());
+
 app.use("/api", userRouter);
 app.use("/api/notes", notesRouter);
 mongoose
